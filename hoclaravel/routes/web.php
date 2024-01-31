d36691a1305fb43817360aa19f2a3c17806529eb
Hoai <hoai.nguyen25@student.passerellesnumeriques.org>
2024-01-31 10:10:57 +0700

    Laravel-Bai4

--- hoclaravel/routes/web.php
+++ hoclaravel/routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Models\User;
+use Illuminate\Http\Request;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,14 +14,74 @@ use App\Models\User;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', function(){
+    $html = '<h1> Học lập trình tại Unicode</h1>';
+    return $html;
+    // return ('Home Page');
 });
 
-Route::get('/unicode', function () {
-    return view('home');
-});
+// Route::get('/unicode', function (){
+//     return view('form');
+//     //return 'Phương thức Get của path/unicode';
+// });
 
-Route::get('/san-pham', function () {
-    return view('product');
+// Route::post('/unicode', function(){
+//     return 'Phương thức Post của path/unicode';
+// });
+
+// Route::put('/unicode', function(){
+//     return 'Phương thức Put của path/unicode';
+// });
+
+// Route::delete('/unicode', function(){
+//     return 'Phương thức Delete của path/unicode';
+// });
+
+// Route::patch('/unicode', function(){
+//     return 'Phương thức Patch của path/unicode';
+// });
+
+// Route::match(['get', 'post'], '/unicode', function(){
+//     return $_SERVER['REQUEST_METHOD'];
+// });
+
+// Route::any('/unicode', function(Request $request){
+//     return $request->method();
+//     //return $_SERVER['REQUEST_METHOD'];
+// });
+
+// Route::get('show-form', function(){
+//     return view('form');
+// });
+
+// Route::redirect('/unicode', 'show-form');
+
+// Route::view('show-form', 'form'); // chỉ sử cho phương thức GET 
+
+Route::prefix('admin')->group(function(){
+    Route::get('/unicode', function (){
+        return 'Phương thức Get của path/unicode';
+    });
+    
+    Route::get('show-form', function(){
+        return view('form');
+    });
+    
+    Route::prefix('products')->group(function(){
+        Route::get('/', function (){
+            return 'Danh sách sản phẩm';
+        });
+
+        Route::get('/add', function (){
+            return 'Thêm sản phẩm';
+        });
+
+        Route::get('/edit', function (){
+            return 'Sửa sản phẩm';
+        });
+
+        Route::get('/delete', function (){
+            return 'Xóa sản phẩm';
+        });
+    });
 });
