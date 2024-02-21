38673e1ea6b2b143201a1781b34106b90378c62c
Hoai <hoai.nguyen25@student.passerellesnumeriques.org>
2024-02-22 00:09:23 +0700

    Laravel-Bai5

--- hoclaravel/app/Http/Controllers/HomeController.php
+++ hoclaravel/app/Http/Controllers/HomeController.php
@@ -6,8 +6,16 @@ use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    //
+    //Action index()
     public function index(){
-        
+        return 'Home';
+    }
+    
+    //Action getNews()
+    public function getNews(){
+        return 'Danh sách tin tức ';
+    }
+    public function getCategories($id){
+        return 'Chuyên mục: '.$id;
     }
 }
