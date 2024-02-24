c0f44e8af102124d09b79cd1c8550e55eeddb21f
Hoai <hoai.nguyen25@student.passerellesnumeriques.org>
2024-02-24 17:52:11 +0700

    Laravel-Bai8

--- hoclaravel/app/Http/Controllers/HomeController.php
+++ hoclaravel/app/Http/Controllers/HomeController.php
@@ -3,12 +3,36 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\View;
+
 
 class HomeController extends Controller
 {
     //Action index()
     public function index(){
-        return 'Home';
+        $title = "Học lập trình web tại Unicode";
+        $content = 'Học lập trình Laravel 8.x tại Unicode';
+        /*
+        [
+            'title' => $title,
+            'content' => $content
+        ]
+        compact('title', 'content')
+        */
+        return view('home')->with(['title'=>$title, 'content'=>$content]);
+
+        // $dataView = [
+        //     'titleData' => $title,
+        //     'contentData' => $content
+        // ];
+        // return view('home', $dataView); //load view home.php
+
+        // return View::make('home')->with(['title'=>$title, 'content'=>$content]);
+
+        // $contentView = view('home');
+        // // $contentView = $contentView ->render(); //chuyển chuỗi thành html thô -> in pdf
+        // dd($contentView);
+        // return $contentView;
     }
     
     //Action getNews()
@@ -17,5 +41,9 @@ class HomeController extends Controller
     }
     public function getCategories($id){
         return 'Chuyên mục: '.$id;
+    }
+
+    public function getProductDetail($id){
+        return view('clients/products/detail', compact('id'));
     }
 }
