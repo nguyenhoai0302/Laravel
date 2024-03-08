748ecf89a9b67b0096a55751992c13b60e133a5d
Hoai <hoai.nguyen25@student.passerellesnumeriques.org>
2024-03-08 16:00:09 +0700

    Laravel-Bai21

--- hoclaravel/routes/web.php
+++ hoclaravel/routes/web.php
@@ -51,38 +51,38 @@ Route::get('/', [HomeController::class, 'index']);
 Route::get('/san-pham', [HomeController::class, 'products'])->name('product');
 Route::get('san-pham/{id}', [HomeController::class, 'getProductDetail']);
 Route::get('them-san-pham', [HomeController::class, 'getAdd']);
-// Route::post('them-san-pham', [HomeController::class, 'postAdd']);
+Route::post('them-san-pham', [HomeController::class, 'postAdd']);
 Route::put('them-san-pham', [HomeController::class, 'putAdd']);
 
-Route::get('/demo-response', function() {
-    $contentArr = [
-        'name' => 'Laravel 8.x',
-        'lesson' => 'Khóa học lập trinh Laravel',
-        'academy' => 'Unicode Academy'
-    ];
-    return $contentArr;
-});
+// Route::get('/demo-response', function() {
+//     $contentArr = [
+//         'name' => 'Laravel 8.x',
+//         'lesson' => 'Khóa học lập trinh Laravel',
+//         'academy' => 'Unicode Academy'
+//     ];
+//     return $contentArr;
+// });
 
-Route::get('lay-thong-tin', [Home1Controller::class, 'getArr']);
-Route::get('/demo-response', function () {
-    // $contentArr = ['name' => 'Unicode', 'version' => 'Laravel 8.x', 'lesson' => 'HTTP Respon Laravel'];
-    // return $contentArr;
-    // return response() -> json ($contentArr)->header("Api-Key", '1234');
+// Route::get('lay-thong-tin', [Home1Controller::class, 'getArr']);
+// Route::get('/demo-response', function () {
+//     // $contentArr = ['name' => 'Unicode', 'version' => 'Laravel 8.x', 'lesson' => 'HTTP Respon Laravel'];
+//     // return $contentArr;
+//     // return response() -> json ($contentArr)->header("Api-Key", '1234');
 
-    return view('clients.demo-test');
-})->name('demo-response');
+//     return view('clients.demo-test');
+// })->name('demo-response');
 
-Route::post('/demo-response', function(Request $request){
-    if(!empty($request->username)){
-        // return redirect()->route('demo-response');
-        // return route('demo-response'); // truyền tên vào
-        return back()->withInput() -> with('mess', 'Validate thành công'); //lấy ra các danh sách vừa input
-    }
-    return redirect(route('demo-response'))-> with('mess', 'Validate không thành công'); // with -> truyền một thông báo
-});
+// Route::post('/demo-response', function(Request $request){
+//     if(!empty($request->username)){
+//         // return redirect()->route('demo-response');
+//         // return route('demo-response'); // truyền tên vào
+//         return back()->withInput() -> with('mess', 'Validate thành công'); //lấy ra các danh sách vừa input
+//     }
+//     return redirect(route('demo-response'))-> with('mess', 'Validate không thành công'); // with -> truyền một thông báo
+// });
 
-Route::get('download-image', [Home1Controller::class, 'downloadImage'])->name('download-image');
-Route::get('download-doc', [Home1Controller::class, 'downloadDoc'])->name('download-doc');
+// Route::get('download-image', [Home1Controller::class, 'downloadImage'])->name('download-image');
+// Route::get('download-doc', [Home1Controller::class, 'downloadDoc'])->name('download-doc');
 
 // Admin Route
 // Route::middleware('auth.admin')->prefix('admin')->group(function () {
